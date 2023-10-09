8552b048f61a3e4a8d6b140fa8ed3d9ed5711f2b
Shreyansh <Shreyanshsahay04071992@gmail.com>
2023-10-09 18:36:49 +0530

    Push the code

--- Check.py
+++
@@ -1,7 +0,0 @@
-table_dict={
-    'emp_manager':["emp_id","mp_name","salary","manager_id"],
-    'RankTest1':["Name","Sales","Location"]}
-
-for key in table_dict.values():
-    values = table_dict[key]
-    print(f"Key: {key}, Values: {values}")
